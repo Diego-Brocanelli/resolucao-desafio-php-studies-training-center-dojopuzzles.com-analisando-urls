09407d79f5f74ed90e0572a81f946d00b0fb7aac
Diego Brocanelli <diegod2@msn.com>
2017-10-21 12:51:42 -0200

    ampliação da cobertura dos testes e 100% de coverage.

--- tests/URLAnalyzerTest.php
+++ tests/URLAnalyzerTest.php
@@ -60,10 +60,10 @@ final class URLAnalyzerTest extends TestCase
      *
      * @return void
      */
-    public function testGetParamters(): void
+    public function testGetParameters(): void
     {
-        $this->assertEquals( true, array_key_exists('user', $this->analyzer->getParamters()) );
-        $this->assertEquals( 'fulano', $this->analyzer->getParamters()['user'] );
+        $this->assertEquals( true, array_key_exists('user', $this->analyzer->getParameters()) );
+        $this->assertEquals( 'fulano', $this->analyzer->getParameters()['user'] );
     }
 
     /**
@@ -92,5 +92,61 @@ final class URLAnalyzerTest extends TestCase
         $analyzer = new URLAnalyzer($url);
 
         $this->assertEquals('undefined', $analyzer->getHost());
+    }
+
+    /**
+     * Test undefind host
+     *
+     * @return void
+     */
+    public function testEmptyHost()
+    {
+        $url = new Url('google.com/mail?user=fulano');
+        
+        $analyzer = new URLAnalyzer($url);
+
+        $this->assertEquals('', $analyzer->getHost());
+    }
+
+    /**
+     * Test undefind host
+     *
+     * @return void
+     */
+    public function testEmptyParameters()
+    {
+        $url = new Url('http://www.google.com/mail');
+        
+        $analyzer = new URLAnalyzer($url);
+
+        $this->assertEquals([], $analyzer->getParameters());
+    }
+
+    /**
+     * Test undefind path
+     *
+     * @return void
+     */
+    public function testEmptyPath()
+    {
+        $url = new Url('http://www.google.com');
+        
+        $analyzer = new URLAnalyzer($url);
+
+        $this->assertEquals('', $analyzer->getPath());
+    }
+
+    /**
+     * Test undefind domain
+     *
+     * @return void
+     */
+    public function testEmptyDomain()
+    {
+        $url = new Url('http://');
+        
+        $analyzer = new URLAnalyzer($url);
+
+        $this->assertEquals('', $analyzer->getDomain());
     }
 }
